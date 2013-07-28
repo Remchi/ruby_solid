9a7ce1add141542739535cdd6243e386a7a7ff20
Rem Zolotykh <remchi@gmail.com>
2013-07-28 14:42:14 +0300

    AttackAction effect

--- lib/attack_action.rb
+++ lib/attack_action.rb
@@ -7,7 +7,13 @@ class AttackAction
   end
 
   def activate(target)
-    dicepool.skill_check(owner.strength, target.toughness)
+    if dicepool.skill_check(owner.strength, target.toughness)
+      target.kill
+      owner.gain_exp
+      owner.gain_gold
+    else
+      owner.damage(target.damage)
+    end
   end
 
 end
--- spec/lib/attack_action_spec.rb
+++ spec/lib/attack_action_spec.rb
@@ -2,14 +2,42 @@ require 'spec_helper'
 require_relative '../../lib/attack_action'
 
 describe AttackAction do
-  let(:hero) { double("hero", strength: 3)}
+  let(:hero) { double("hero", strength: 3, gain_exp: nil, gain_gold: nil, damage: nil)}
   let(:dicepool) { double("dicepool") }
   let(:action) { AttackAction.new hero, dicepool}
+  let(:monster) { double("monster", toughness: 2, kill: nil, damage: 4) }
+
+  describe "effect" do
+    context "success" do
+      before :each do
+        dicepool.stub(:skill_check).and_return(true)
+      end
+
+      it "kills monster" do
+        monster.should_receive(:kill)
+        action.activate(monster)
+      end
+      it "rewards owner with exp" do
+        hero.should_receive(:gain_exp)
+        action.activate(monster)
+      end
+      it "rewards owner with gold" do
+        hero.should_receive(:gain_gold)
+        action.activate(monster)
+      end
+    end
+    context "failure" do
+      it "damages owner" do
+        dicepool.stub(:skill_check).and_return(false)
+        hero.should_receive(:damage).with(monster.damage)
+        action.activate(monster)
+      end
+    end
+  end
 
   describe "activate" do
 
     it "makes strength check against target toughness" do
-      monster = double("monster", toughness: 2)
       dicepool.should_receive(:skill_check).with(hero.strength, monster.toughness)
       action.activate(monster)
     end
