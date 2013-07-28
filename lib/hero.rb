57f36365dc7d994b0eb575fc2de045a86f8fc527
Rem Zolotykh <remchi@gmail.com>
2013-07-28 14:51:05 +0300

    Add needed attributes and methods to Hero class for AttackAction to work

--- lib/hero.rb
+++ lib/hero.rb
@@ -1,14 +1,30 @@
 class Hero
-  attr_reader :strength, :health, :actions
+  attr_reader :strength, :health, :actions, :gold, :exp
 
   def initialize(attr = {})
     @strength = attr.fetch(:strength, 3)
     @health = attr.fetch(:health, 10)
     @actions = attr.fetch(:actions, {})
+
+    @gold = 0
+    @exp = 0
   end
 
   def activate_action(action_name, target)
     actions[action_name].activate(target)
   end
+
+  def damage(hit)
+    @health -= hit
+  end
+
+  def gain_gold(amount)
+    @gold += amount
+  end
+
+  def gain_exp(amount)
+    @exp += amount
+  end
+
 
 end
--- spec/lib/hero_spec.rb
+++ spec/lib/hero_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 require_relative '../../lib/hero'
 
 describe Hero do
+  let(:hero) { Hero.new }
   describe "default attributes" do
-    let(:hero) { Hero.new }
 
     it "has default strength equal to 3" do
       expect(hero.strength).to eq(3)
@@ -11,7 +11,32 @@ describe Hero do
     it "has default health equal to 10" do
       expect(hero.health).to eq(10)
     end
+    it "has default gold equal to 0" do
+      expect(hero.gold).to eq(0)
+    end
+    it "has default exp equal to 0" do
+      expect(hero.exp).to eq(0)
+    end
   end
+
+  it "can be damaged" do
+    hero.damage(3)
+    hero.damage(2)
+    expect(hero.health).to eq(5)
+  end
+
+  it "gains gold" do
+    hero.gain_gold(10)
+    hero.gain_gold(5)
+    expect(hero.gold).to eq(15)
+  end
+
+  it "gains exp" do
+    hero.gain_exp(10)
+    hero.gain_exp(5)
+    expect(hero.exp).to eq(15)
+  end
+
 
   it "can be initialized with custom strength" do
     hero = Hero.new strength: 3
